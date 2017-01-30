b9b43d738b75bc69aac89a12268b86c75bda6d67
Dustin Largent <dustin638@gmail.com>
2017-01-30 09:51:31 -0500

    license, adding token feature

--- mirabell.py
+++ mirabell.py
@@ -4,8 +4,8 @@ import pydle
 import json
 import logging
 import importlib
-import pprint
 
+loggingFormat = '%(asctime)s %(levelname)s:%(name)s: %(message)s'
 logging.basicConfig(level=logging.DEBUG)
 
 config = json.load(open("config.json"))
@@ -19,22 +19,34 @@ class Mirabell(BaseClient):
 
     def __init__(self, nick, *args, **kwargs):
         super().__init__(nick, *args, **kwargs)
-        # join main channel defined in config
-        self.channel = config['channel']
+        self.channel = config['channel']  # Main channel set in config
+        self.currentChannels = []  # List of current channels the bot is in
 
     def on_connect(self):
         super().on_connect()
-        # message.nick('NickServ', 'identify ' + config['nickserv_password'])
         self.join(self.channel)
         # join any other channels you want, defined in config
         for chan in config['auxchans']:
             self.join(chan)
+            self.currentChannels.append(chan)
 
     @pydle.coroutine
-    def is_admin(self, nickname):
+    def changeToken(self, message, source):
+        if len(message.split()) > 2:
+            return "Token cannot contain spaces."
+        token = message.split(' ')[1]
+        if not token:
+            return "Token must not be empty."
+        admin = yield self.isAdmin(source)
 
+        if admin:
+            return "Token changed to " + token
+        else:
+            return "You must be an admin to change the token."
+
+    @pydle.coroutine
+    def isAdmin(self, nickname):
         admin = False
-
         if nickname in config['admin']:
             admin = True
         return admin
@@ -43,26 +55,33 @@ class Mirabell(BaseClient):
     def on_message(self, target, source, message):
         super().on_message(target, source, message)
 
-        # Tell a user if they are an administrator
-        if message.startswith(config['token'] + "admin"):
-            admin = yield self.is_admin(source)
-            if admin:
-                self.message(target, source + ': You are an administrator.')
-            else:
-                self.message(target, source + ': You are not an administrator.')
-
+        # Display a list of all bot features.
         if message.startswith(config['token'] + "help"):
             features = [
                 "admin: tell a user if they have admin privileges or not.",
-                "stuff: does stuff"
+                "token: change bot token if admin"
             ]
             self.message(target, "Here are the things I can do:")
             for feature in features:
                 self.message(target, feature)
 
+        # Tell a user if they are an administrator
+        if message.startswith(config['token'] + "admin"):
+            admin = yield self.isAdmin(source)
+            if admin:
+                self.message(target, source + ': You are an administrator.')
+            else:
+                self.message(target, source + ': You are not an administrator.')
+
+        # Change the preceding character before commands
+        if message.startswith(config['token'] + "token"):
+            response = yield self.changeToken(message, source)
+            self.message(target, response)
+
 
 client = Mirabell(config['nick'], sasl_username=config['nickserv_username'], sasl_password=config['nickserv_password'])
 client.connect(config['server'], config['port'], tls=config['tls'])
+
 try:
     client.handle_forever()
 except KeyboardInterrupt:
